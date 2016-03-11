284317c47ddc9cca6c69a62c5a4e2761051558cf
Robert Hajek <hajek.rob@gmail.com>
2016-03-10 21:12:41 -0700

    minor stylings

--- src/components/Scheduler.jsx
+++ src/components/Scheduler.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 
 const Scheduler = (props) => {
   return (
-    <div>
+    <div className='schedule-container'>
       <div className='start-time col-xs-4'>
         <div className='hour'>
-          {props.start}
+          {props.start.time}
+        <span className='meridiem'>{props.start.meridiem}</span>
         </div>
       </div>
       <div className="hour col-xs-2"> - </div>
       <div className='end-time col-xs-4'>
         <div className='hour'>
-          {props.end}
+          {props.end.time}
+          <span className='meridiem'>{props.end.meridiem}</span>
         </div>
       </div>
       <img className='clock col-xs-2' src='../style/images/clock.png'></img>
